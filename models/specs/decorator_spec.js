c013b54023bc457a74f52fdae33bdbcd6c34b793
Jonny Reading <jonnyreading@gmail.com>
2020-08-18 15:20:11 +0100

    tests written for decorator

---
+++ models/specs/decorator_spec.js
@@ -0,0 +1,51 @@
+const assert = require("assert");
+const Decorator = require("../decorator.js");
+const Room = require("../room.js");
+const Paint = require("../paint.js");
+
+describe("Decorator", function () {
+    let decorator, paint1, paint2, bigRoom, smallRoom;
+    beforeEach(function () {
+        decorator = new Decorator();
+        paint1 = new Paint(12);
+        paint2 = new Paint(10);
+        bigRoom = new Room(2000);
+        smallRoom = new Room(15);
+    });
+    it("should have an empty paint stock", function () {
+        const actual = decorator.stock;
+        assert.deepStrictEqual(actual, []);
+    });
+    it("should add a can of paint to stock", function () {
+        decorator.addCan(paint1);
+        const actual = decorator.stock;
+        assert.deepStrictEqual(actual, [paint1]);
+    });
+    it("should have total litres in stock", function () {
+        decorator.addCan(paint1);
+        decorator.addCan(paint2);
+        const actual = decorator.totalLitres();
+        assert.strictEqual(actual, 22);
+    });
+    it("should be able to paint small room", function () {
+        decorator.addCan(paint1);
+        decorator.addCan(paint2);
+        const actual = decorator.canPaint(smallRoom);
+        assert.strictEqual(actual, true);
+    });
+    it("should not be able to paint big room", function () {
+        decorator.addCan(paint1);
+        decorator.addCan(paint2);
+        const actual = decorator.canPaint(bigRoom);
+        assert.strictEqual(actual, false);
+    });
+    it("can paint small room with paint left", function () {
+        decorator.addCan(paint1);
+        decorator.addCan(paint2);
+        decorator.paint(smallRoom);
+        let actual = decorator.totalLitres();
+        assert.strictEqual(actual, 7);
+        let actual = smallRoom.painted;
+        assert.strictEqual(actual, true);
+    });
+});
